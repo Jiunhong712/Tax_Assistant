556840d64a66f5847ce01c115a82eeaabc82695b
JiunHongLim <xavier4666@gmail.com>
2025-05-16 22:41:41 +0800

    Added fonts and logo image

---
+++ taxassistant/lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// Define custom colors
+const Color kColorPrimary = Color(
+  0xFF31474A,
+); // Dark color for text/primary elements
+const Color kColorAccent = Color(
+  0xFF7ED2AA,
+); // Accent color for buttons/highlights
+const Color kColorBackground = Color(0xFFCFEBF2); // Light color for backgrounds
